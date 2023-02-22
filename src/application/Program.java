e7323155385be1dc1ec834db9aa472c862339793
Paulo <paulo_vinicius_21@hotmail.com>
2023-02-22 12:47:37 -0300

    Programa que receba um nome completo na forma de uma String e mostre a abreviatura deste nome.

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite uma string: ");
+		String string1 = sc.next();
+		System.out.println("Digite outra string: ");
+		String string2 = sc.next();
+		
+	}
+
+}
